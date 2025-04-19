3f75e3c58e762d2a42e7b7f96d28e50bef788ea2
franklyngomes <franklyngomes15@gmail.com>
2025-04-19 10:35:06 +0530

    cabHop

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,27 +1,45 @@
-import { NextRequest, NextResponse } from 'next/server';
-import Stripe from 'stripe';
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
 
-const stripeKey = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!;
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2025-03-31.basil",
+});
 
-if(!stripeKey){
-  throw new Error("Missing STRIPE_SECRET_KEY in environment variables.")
-}
-const stripe = new Stripe(stripeKey, {
-  apiVersion: '2025-03-31.basil',
-})
 export async function POST(req: NextRequest) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const body = await req.json();
+
+  if (body.amount < 50) {
+    return NextResponse.json(
+      { error: "Amount must be at least $0.50" },
+      { status: 400 }
+    );
+  }
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: body.amount,
-      currency: 'usd',
+      currency: "usd",
       automatic_payment_methods: { enabled: true },
+      metadata: {
+        userId,
+      },
     });
 
-    return NextResponse.json({ clientSecret: paymentIntent.client_secret });
-  } catch (error : any) {
-    console.error("❌ Stripe error:", error.message);
-    return NextResponse.json({ error: 'Failed to create payment intent' }, { status: 500 });
+    return NextResponse.json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err: any) {
+    console.error("❌ Stripe error:", err.message);
+    return NextResponse.json(
+      { error: "Failed to create payment intent" },
+      { status: 500 }
+    );
   }
 }
